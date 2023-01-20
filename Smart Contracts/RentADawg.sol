be88e474c78e7143671a0f0add259f31fa0533ec
JackBDart <spamme385@gmail.com>
2023-01-20 15:37:01 -0800

    actually pick up changes

--- Smart Contracts/RentADawg.sol
+++ Smart Contracts/RentADawg.sol
@@ -8,209 +8,192 @@ import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "./lib/IBAYCSewerPassClaim.sol";
 import "./lib/IDelegationRegistry.sol";
 
-contract BorroMyDoggo is IERC721Receiver, ReentrancyGuard {
+contract RentADawg is IERC721Receiver, ReentrancyGuard {
     using BitMaps for BitMaps.BitMap;
 
-    address constant public SEWER_PASS = 0x764AeebcF425d56800eF2c84F2578689415a2DAa;
-    address constant public SEWER_PASS_CLAIM = 0xBA5a9E9CBCE12c70224446C24C111132BECf9F1d;
     address constant public BAYC = 0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D;
     address constant public MAYC = 0x60E4d786628Fea6478F785A6d7e704777c86a7c6;
     address constant public BAKC = 0xba30E5F9Bb24caa003E9f2f0497Ad287FDF95623;
-    address constant public FOOBAR = 0xe5ee2B9d5320f2D1492e16567F36b578372B3d9F;
-    address constant public THOMAS = 0x3e6a203ab73C4B35Be1F65461D88Fb21DE26446e;
-    uint64 constant public LENDER_FEE = 90;
+    address constant public BAKC = 0xba30E5F9Bb24caa003E9f2f0497Ad287FDF95623;
+
+    address payable constant public LAYERRXYZ = 0x936Dd8afE0ca93BE3fadbb4B1c4BF1735e8b57da;
+    address payable constant public PAT = 0xE9bC3058A30E14C2Ba6a4fD6B49684237A67aF56;
+    address payable constant public JUSTADEV = 0x3e6a203ab73C4B35Be1F65461D88Fb21DE26446e;
+    uint64 constant public FEE = 50;
+
     IDelegationRegistry delegateCash = IDelegationRegistry(0x00000000000076A84feF008CDAbe6409d2FE638B);
 
-    BitMaps.BitMap private doggoLoaned;
-    mapping(uint256 => uint256) public borroCost;
-    address private currentMinter;
+    address constant public SEWER_PASS = 0x764AeebcF425d56800eF2c84F2578689415a2DAa;
+    address constant public SEWER_PASS_CLAIM = 0xBA5a9E9CBCE12c70224446C24C111132BECf9F1d;
 
-    /** borroDoggos allows bayc/mayc holder to mint tier 4/tier 2 sewer pass
+    uint256 public layerrEarnings = 0;
+
+    mapping(uint256 => uint256) public rentACost;
+    address private currentMinter;
+    BitMaps.BitMap private dawgLoaned;
+
+    /** rentADawgs allows bayc/mayc holder to mint tier 4/tier 2 sewer pass
         apes must be delegated to this contract address by ape owner using delegate.cash
-        using doggo delegated and loaned by a doggo holder
-        payment must be greater than or equal to sum of all doggos used and can be calculated with calculateBorroCost
+        using dawg delegated and loaned by a dawg holder
+        payment must be greater than or equal to sum of all dawgs used and can be calculated with calculateRentACost
         sewer passes will be minted and transfered to the account that calls this function
         minter must be direct owner or delegate for the BAYC/MAYC tokens supplied
-        BAYC or MAYC can be supplied as empty arrays but total apes must equal total doggos
-        90% of borro fees go to doggo owner, 5% to 0xfoobar for delegate.cash and 5% to 0xth0mas
+        BAYC or MAYC can be supplied as empty arrays but total apes must equal total dawgs
+        90% of rentA fees go to dawg owner, 5% to 0xfoobar for delegate.cash and 5% to 0xth0mas
     */
-    function borroDoggos(uint256[] calldata baycIds, uint256[] calldata maycIds, uint256[] calldata doggoIds) external payable nonReentrant {
-        require((baycIds.length + maycIds.length) == doggoIds.length, "APE/DOGGO COUNT MISMATCH");
-        uint256 totalBorroCost = this.calculateBorroCost(doggoIds);
-        require(msg.value >= totalBorroCost, "INSUFFICIENT PAYMENT");
-        uint256 doggoIndex = 0;
+    function rentADawgs(uint256[] calldata baycIds, uint256[] calldata maycIds, uint256[] calldata dawgIds) external payable nonReentrant {
+        require((baycIds.length + maycIds.length) == dawgIds.length, "APE/DAWG COUNT MISMATCH");
+        uint256 totalRentACost = this.calculateRentACost(dawgIds);
+        require(msg.value >= totalRentACost, "INSUFFICIENT PAYMENT");
+        uint256 dawgIndex = 0;
         currentMinter = msg.sender;
         address apeOwner;
         for(uint256 i = 0;i < baycIds.length;i++) {
             apeOwner = IERC721(BAYC).ownerOf(baycIds[i]);
             require(apeOwner == msg.sender ||
                 delegateCash.checkDelegateForToken(msg.sender, apeOwner, BAYC, baycIds[i]), "NOT APE OWNER OR DELEGATE");
-            IBAYCSewerPassClaim(SEWER_PASS_CLAIM).claimBaycBakc(baycIds[i], doggoIds[doggoIndex]);
-            payDoggoOwner(doggoIds[doggoIndex]);
-            doggoIndex++;
+            IBAYCSewerPassClaim(SEWER_PASS_CLAIM).claimBaycBakc(baycIds[i], dawgIds[dawgIndex]);
+            payDawgOwner(dawgIds[dawgIndex]);
+            dawgIndex++;
         }
         for(uint256 i = 0;i < maycIds.length;i++) {
             apeOwner = IERC721(MAYC).ownerOf(maycIds[i]);
             require(apeOwner == msg.sender ||
                 delegateCash.checkDelegateForToken(msg.sender, apeOwner, MAYC, maycIds[i]), "NOT APE OWNER OR DELEGATE");
-            IBAYCSewerPassClaim(SEWER_PASS_CLAIM).claimMaycBakc(maycIds[i], doggoIds[doggoIndex]);
-            payDoggoOwner(doggoIds[doggoIndex]);
-            doggoIndex++;
+            IBAYCSewerPassClaim(SEWER_PASS_CLAIM).claimMaycBakc(maycIds[i], dawgIds[dawgIndex]);
+            payDawgOwner(dawgIds[dawgIndex]);
+            dawgIndex++;
         }
         currentMinter = address(0);
     }
 
-    /** friendly utility function for ape holders to bulk mint sewer passes
-        apes & doggos must be delegated to this contract address by owner using delegate.cash
-        doggos used for tier 4 sewer passes first, when doggo count is exceeded tier 3 passes get minted
-        if doggos are left after bayc sewer passes, doggos used to mint tier 2 sewer passes
-        if maycs are left after doggo count is exceeded, tier 1 passes are minted
-        donations appreciated but not required to use
+    /** calculate and send payment for use of dawg in minting sewer pass, cleans up state
     */
-    function bulkMintSewerPass(uint256[] calldata baycIds, uint256[] calldata maycIds, uint256[] calldata doggoIds) external payable nonReentrant {
-        uint256 doggoIndex = 0;
-        currentMinter = msg.sender;
-        address apeOwner;
-        address doggoOwner;
-        for(uint256 baycIndex = 0;baycIndex < baycIds.length;baycIndex++) {
-            apeOwner = IERC721(BAYC).ownerOf(baycIds[baycIndex]);
-            require(apeOwner == msg.sender ||
-                delegateCash.checkDelegateForToken(msg.sender, apeOwner, BAYC, baycIds[baycIndex]), "NOT APE OWNER OR DELEGATE");
-            if(doggoIndex >= doggoIds.length) {
-                IBAYCSewerPassClaim(SEWER_PASS_CLAIM).claimBayc(baycIds[baycIndex]);
-            } else {
-                doggoOwner = IERC721(BAKC).ownerOf(doggoIds[doggoIndex]);
-                require(doggoOwner == msg.sender ||
-                    delegateCash.checkDelegateForToken(msg.sender, doggoOwner, BAKC, doggoIds[doggoIndex]), "NOT DOGGO OWNER OR DELEGATE");
-                IBAYCSewerPassClaim(SEWER_PASS_CLAIM).claimBaycBakc(baycIds[baycIndex], doggoIds[doggoIndex]);
-                doggoIndex++;
-            }
-        }
-        for(uint256 maycIndex = 0;maycIndex < maycIds.length;maycIndex++) {
-            apeOwner = IERC721(MAYC).ownerOf(maycIds[maycIndex]);
-            require(apeOwner == msg.sender ||
-                delegateCash.checkDelegateForToken(msg.sender, apeOwner, MAYC, maycIds[maycIndex]), "NOT APE OWNER OR DELEGATE");
-            if(doggoIndex >= doggoIds.length) {
-                IBAYCSewerPassClaim(SEWER_PASS_CLAIM).claimMayc(maycIds[maycIndex]);
-            } else {
-                doggoOwner = IERC721(BAKC).ownerOf(doggoIds[doggoIndex]);
-                require(doggoOwner == msg.sender ||
-                    delegateCash.checkDelegateForToken(msg.sender, doggoOwner, BAKC, doggoIds[doggoIndex]), "NOT DOGGO OWNER OR DELEGATE");
-                IBAYCSewerPassClaim(SEWER_PASS_CLAIM).claimMaycBakc(maycIds[maycIndex], doggoIds[doggoIndex]);
-                doggoIndex++;
-            }
-        }
-        currentMinter = address(0);
-    }
-
-    /** calculate and send payment for use of doggo in minting sewer pass, cleans up state
-    */
-    function payDoggoOwner(uint256 doggoId) internal {
-        address doggoOwner = IERC721(BAKC).ownerOf(doggoId);
-        uint256 payment = borroCost[doggoId] * LENDER_FEE / 100;
-        (bool sent, ) = payable(doggoOwner).call{value: payment}("");
+    function payDawgOwner(uint256 dawgId) internal {
+        address dawgOwner = IERC721(BAKC).ownerOf(dawgId);
+        uint256 payment = rentACost[dawgId] * FEE / 100;
+        (bool sent, ) = payable(dawgOwner).call{value: payment}("");
         require(sent);
-        borroCost[doggoId] = 0;
-        doggoLoaned.unset(this.getDoggoLoanIndex(doggoOwner, doggoId));
+        rentACost[dawgId] = 0;
+        dawgLoaned.unset(this.getDawgLoanIndex(dawgOwner, dawgId));
     }
 
-    /** withdraw fees for 0xfoobar and 0xth0mas
-    */
-    function withdraw() external {
-        uint256 feesCollected = address(this).balance;
-        uint256 foobarShare = feesCollected / 2;
-        (bool fbSent, ) = payable(FOOBAR).call{value: foobarShare}("");
-        require(fbSent);
-        (bool tSent, ) = payable(THOMAS).call{value: (feesCollected -foobarShare)}("");
-        require(tSent);
-    }
 
-    /** loan doggos for bayc/mayc to mint higher tier sewer passes
-        doggos must be delegated to this contract address by doggo owner using delegate.cash
-        doggoIds = array of doggos to loan out, must be direct owner or delegate to call
-        costToBorro = payment to be received when your doggo is used to mint a sewer pass, cost is in WEI
-        payment will be sent to doggo owner wallet
-        can be called again to adjust costToBorro
+    /** takes dawg off loan, you can also revoke delegation to this contract with delegate.cash for same effect
     */
-    function loanDoggos(uint256[] calldata doggoIds, uint256 costToBorro) external {
-        address doggoOwner;
-        for(uint256 doggoIndex = 0;doggoIndex < doggoIds.length;doggoIndex++) {
-            doggoOwner = IERC721(BAKC).ownerOf(doggoIds[doggoIndex]);
-            require(doggoOwner == msg.sender ||
-                delegateCash.checkDelegateForToken(msg.sender, doggoOwner, BAKC, doggoIds[doggoIndex]), "NOT DOGGO OWNER OR DELEGATE");
-            doggoLoaned.set(this.getDoggoLoanIndex(doggoOwner, doggoIds[doggoIndex]));
-            borroCost[doggoIds[doggoIndex]] = costToBorro;
+    function unloanDawgs(uint256[] calldata dawgIds) external {
+        address dawgOwner;
+        for(uint256 dawgIndex = 0;dawgIndex < dawgIds.length;dawgIndex++) {
+            dawgOwner = IERC721(BAKC).ownerOf(dawgIds[dawgIndex]);
+            require(dawgOwner == msg.sender ||
+                delegateCash.checkDelegateForToken(msg.sender, dawgOwner, BAKC, dawgIds[dawgIndex]), "NOT DAWG OWNER OR DELEGATE");
+            dawgLoaned.unset(this.getDawgLoanIndex(dawgOwner, dawgIds[dawgIndex]));
+            rentACost[dawgIds[dawgIndex]] = 0;
         }
     }
 
-    /** takes doggo off loan, you can also revoke delegation to this contract with delegate.cash for same effect
+
+    /** loan dawgs for bayc/mayc to mint higher tier sewer passes
+        dawgs must be delegated to this contract address by dawg owner using delegate.cash
+        dawgIds = array of dawgs to loan out, must be direct owner or delegate to call
+        costToRentA = payment to be received when your dawg is used to mint a sewer pass, cost is in WEI
+        payment will be sent to dawg owner wallet
+        can be called again to adjust costToRentA
     */
-    function unloanDoggos(uint256[] calldata doggoIds) external {
-        address doggoOwner;
-        for(uint256 doggoIndex = 0;doggoIndex < doggoIds.length;doggoIndex++) {
-            doggoOwner = IERC721(BAKC).ownerOf(doggoIds[doggoIndex]);
-            require(doggoOwner == msg.sender ||
-                delegateCash.checkDelegateForToken(msg.sender, doggoOwner, BAKC, doggoIds[doggoIndex]), "NOT DOGGO OWNER OR DELEGATE");
-            doggoLoaned.unset(this.getDoggoLoanIndex(doggoOwner, doggoIds[doggoIndex]));
-            borroCost[doggoIds[doggoIndex]] = 0;
+    function loanDawgs(uint256[] calldata dawgIds, uint256 costToRentA) external {
+        address dawgOwner;
+        for(uint256 dawgIndex = 0;dawgIndex < dawgIds.length;dawgIndex++) {
+            dawgOwner = IERC721(BAKC).ownerOf(dawgIds[dawgIndex]);
+            require(dawgOwner == msg.sender ||
+                delegateCash.checkDelegateForToken(msg.sender, dawgOwner, BAKC, dawgIds[dawgIndex]), "NOT DAWG OWNER OR DELEGATE");
+            dawgLoaned.set(this.getDawgLoanIndex(dawgOwner, dawgIds[dawgIndex]));
+            rentACost[dawgIds[dawgIndex]] = costToRentA;
         }
     }
 
-    struct DoggoLoaned {
-        uint64 doggoId;
-        uint64 borroCost;
+    struct DawgLoaned {
+        uint64 dawgId;
+        uint64 rentACost;
     }
 
-    /** utility function to return list of available doggos for sewer pass minting and cost to borro for each doggo
-        find cheapest doggo ids to borrow and supply array to calculateBorroCost for total cost
+    /** utility function to return list of available dawgs for sewer pass minting and cost to rentA for each dawg
+        find cheapest dawg ids to rentAw and supply array to calculateRentACost for total cost
     */
-    function availableDoggos() external view returns(DoggoLoaned[] memory) {
-        uint256 doggosAvailable = 0;
-        address doggoOwner;
-        for(uint256 doggoIndex = 0;doggoIndex < 10000;doggoIndex++) {
-            try IERC721(BAKC).ownerOf(doggoIndex) returns (address result) { doggoOwner = result; } catch { doggoOwner = address(0); }
-            if(doggoLoaned.get(this.getDoggoLoanIndex(doggoOwner, doggoIndex)) && 
-              delegateCash.checkDelegateForToken(address(this), doggoOwner, BAKC, doggoIndex) &&
-              !IBAYCSewerPassClaim(SEWER_PASS_CLAIM).bakcClaimed(doggoIndex)) {
-                doggosAvailable++;
+    function availableDawgs() external view returns(DawgLoaned[] memory) {
+        uint256 dawgsAvailable = 0;
+        address dawgOwner;
+        for(uint256 dawgIndex = 0;dawgIndex < 10000;dawgIndex++) {
+            try IERC721(BAKC).ownerOf(dawgIndex) returns (address result) { dawgOwner = result; } catch { dawgOwner = address(0); }
+            if(dawgLoaned.get(this.getDawgLoanIndex(dawgOwner, dawgIndex)) && 
+              delegateCash.checkDelegateForToken(address(this), dawgOwner, BAKC, dawgIndex) &&
+              !IBAYCSewerPassClaim(SEWER_PASS_CLAIM).bakcClaimed(dawgIndex)) {
+                dawgsAvailable++;
             }
         }
 
-        DoggoLoaned[] memory loans = new DoggoLoaned[](doggosAvailable);
+        DawgLoaned[] memory loans = new DawgLoaned[](dawgsAvailable);
         uint256 currentIndex = 0;
-        for(uint256 doggoIndex = 0;doggoIndex < 10000;doggoIndex++) {
-            try IERC721(BAKC).ownerOf(doggoIndex) returns (address result) { doggoOwner = result; } catch { doggoOwner = address(0); }
-            if(doggoLoaned.get(this.getDoggoLoanIndex(doggoOwner, doggoIndex)) && 
-              delegateCash.checkDelegateForToken(address(this), doggoOwner, BAKC, doggoIndex) &&
-              !IBAYCSewerPassClaim(SEWER_PASS_CLAIM).bakcClaimed(doggoIndex)) {
-                  DoggoLoaned memory dl;
-                  dl.doggoId = uint64(doggoIndex);
-                  dl.borroCost = uint64(borroCost[doggoIndex]);
+        for(uint256 dawgIndex = 0;dawgIndex < 10000;dawgIndex++) {
+            try IERC721(BAKC).ownerOf(dawgIndex) returns (address result) { dawgOwner = result; } catch { dawgOwner = address(0); }
+            if(dawgLoaned.get(this.getDawgLoanIndex(dawgOwner, dawgIndex)) && 
+              delegateCash.checkDelegateForToken(address(this), dawgOwner, BAKC, dawgIndex) &&
+              !IBAYCSewerPassClaim(SEWER_PASS_CLAIM).bakcClaimed(dawgIndex)) {
+                  DawgLoaned memory dl;
+                  dl.dawgId = uint64(dawgIndex);
+                  dl.rentACost = uint64(rentACost[dawgIndex]);
                 loans[currentIndex] = dl;
                 currentIndex++;
-                if(currentIndex >= doggosAvailable) { break; }
+                if(currentIndex >= dawgsAvailable) { break; }
             }
         }
         return loans;
     }
 
-    /** offset doggoId by owner address for loan mapping,
-        ensures that doggo transfer and subsequent delegation
+    /** offset dawgId by owner address for loan mapping,
+        ensures that dawg transfer and subsequent delegation
         by new owner does not enable a boost sale at previous
         listing
     */
-    function getDoggoLoanIndex(address doggoOwner, uint256 doggoId) external pure returns(uint256 doggoLoanIndex) {
-        doggoLoanIndex = uint256(uint160(doggoOwner)) + doggoId;
+    function getDawgLoanIndex(address dawgOwner, uint256 dawgId) external pure returns(uint256 dawgLoanIndex) {
+        dawgLoanIndex = uint256(uint160(dawgOwner)) + dawgId;
     }
 
-    /** calculates total cost of doggo borrowing
+    /** calculates total cost of dawg rentAwing
     */
-    function calculateBorroCost(uint256[] calldata doggoIds) external view returns(uint256 totalBorroCost) {
-        address doggoOwner;
-        for(uint256 i = 0;i < doggoIds.length;i++) {
-            try IERC721(BAKC).ownerOf(doggoIds[i]) returns (address result) { doggoOwner = result; } catch { doggoOwner = address(0); }
-            require(doggoLoaned.get(this.getDoggoLoanIndex(doggoOwner, doggoIds[i])), "DOGGO NOT LOANED");
-            totalBorroCost += borroCost[doggoIds[i]];
+    function calculateRentACost(uint256[] calldata dawgIds) external view returns(uint256 totalRentACost) {
+        address dawgOwner;
+        for(uint256 i = 0;i < dawgIds.length;i++) {
+            try IERC721(BAKC).ownerOf(dawgIds[i]) returns (address result) { dawgOwner = result; } catch { dawgOwner = address(0); }
+            require(dawgLoaned.get(this.getDawgLoanIndex(dawgOwner, dawgIds[i])), "DAWG NOT LOANED");
+            totalRentACost += rentACost[dawgIds[i]];
+        }
+    }
+
+    function withdraw() external {
+        uint256 balance = address(this).balance;
+        if (layerrEarnings < 5 ether) {
+            if (balance + layerrEarnings < 5 ether) {
+                LAYERRXYZ.transfer(balance);
+                layerrEarnings += balance;
+            } else {
+                uint256 remainingOwed = 5 ether - layerrEarnings;
+                LAYERRXYZ.transfer(remainingOwed);
+                layerrEarnings += remainingOwed;
+                balance -= remainingOwed;
+                uint256 justadev = balance * 3 / 100;
+                uint256 layerrxyz = balance * 25 / 100;
+                uint256 pat = balance - justadev - layerrxyz;
+                JUSTADEV.transfer(justadev);
+                LAYERRXYZ.transfer(layerrxyz);
+                PAT.transfer(pat);
+            }
+        } else {
+            uint256 justadev = balance * 3 / 100;
+            uint256 layerrxyz = balance * 25 / 100;
+            uint256 pat = balance - justadev - layerrxyz;
+            JUSTADEV.transfer(justadev);
+            LAYERRXYZ.transfer(layerrxyz);
+            PAT.transfer(pat);
         }
     }
 
