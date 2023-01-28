a2da88fd3c7908393431bdc067eb0182aadeb376
0xth0mas <35287002+0xth0mas@users.noreply.github.com>
2023-01-28 14:51:00 -0600

    added Ken's address

--- Smart Contracts/LayerrSplitter.sol
+++ Smart Contracts/LayerrSplitter.sol
@@ -7,7 +7,7 @@ contract LayerrSplitter is PaymentSplitter {
 
     address constant public LAYERRXYZ = 0x936Dd8afE0ca93BE3fadbb4B1c4BF1735e8b57da;
     address constant public PAT = 0xE9bC3058A30E14C2Ba6a4fD6B49684237A67aF56;
-    address constant public KEN = 0x0000000000000000000000000000000000000001; //TODO: need ken's wallet
+    address constant public KEN = 0x37074614C6343609D405dB6516606d8A8F95AFD9;
     address constant public JUSTADEV = 0x3e6a203ab73C4B35Be1F65461D88Fb21DE26446e;
     address constant public JOSHONG = 0xaf469C4a0914938e6149CF621c54FB4b1EC0c202;
 
